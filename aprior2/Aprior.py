6ef856451f31cb954ece906781a682499bda5020
Hzk <P76073016@ncku.edu.tw>
2018-10-24 01:34:29 +0800

    1

--- aprior2/Aprior.py
+++ aprior2/Aprior.py
@@ -128,11 +128,36 @@ def gen(L,supportdata,minconfidence = 0.05):
                 calc2(st,H1,supportdata,largeitem,minconfidence);
     return  largeitem;
 
+def calc_confidence(L , supportdata, set1 , set2 ):
+    Count1 = 0;
+    Count2 = 0;
+    for set in L:
+        if (len(set1 & set) == len(set1)):
+            Count1+=1;
+        if (len(set2 & set) == len(set2)):
+            Count2+=1;
+    ret = Count2 * 1.0 / Count1;
+    return ret;
+
+def gen2(L,supportdata,minsup = 0.5):
+    largeitem = [];
+
+    for i in range(1,len(L)):
+        for j in range(i+1,len(L)):
+            combine = L[i] | L[j];
+            if ( (L[i] & L[j]) == 0 and combine in supportdata):
+                confidence = calc_confidence(L,supportdata,L[i],combine);
+                support = supportdata[combine];
+                if support > minsup :
+                    print( L[i],'->',L[j],'con = ',confidence,' sup = ',support);
+                largeitem.append([L[i],L[j],confidence]);
+    return largeitem;
+
+
 
 data  = loadKaggledata();
 L , supportdata = aprior(data,minsup=100);
 rules = gen(L , supportdata , 0);
-
 
 
 for item in rules:
